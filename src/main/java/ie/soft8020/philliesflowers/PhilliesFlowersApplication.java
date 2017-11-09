6532e988eb6b80435496b133d798a813e7ff15d5
Brian Coveney <coveneybrian@gmail.com>
2017-11-09 11:54:45 +0000

    Initial commit

---
+++ src/main/java/ie/soft8020/philliesflowers/PhilliesFlowersApplication.java
@@ -0,0 +1,12 @@
+package ie.soft8020.philliesflowers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhilliesFlowersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhilliesFlowersApplication.class, args);
+	}
+}
