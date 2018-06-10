cb47d8a35eea07a1282efb7528b71b886248aabe
BrianCoveney <coveneybrian@gmail.com>
2018-06-10 09:27:31 +0100

    Update the Controllers

--- src/main/java/ie/soft8020/philliesflowers/controllers/Controllers.java
+++ src/main/java/ie/soft8020/philliesflowers/controllers/Controllers.java
@@ -1,39 +1,39 @@
 package ie.soft8020.philliesflowers.controllers;
 
-import java.util.List;
-
+import ie.soft8020.philliesflowers.entities.Customer;
+import ie.soft8020.philliesflowers.repositories.CustomerRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import ie.soft8020.philliesflowers.entities.Customer;
-import ie.soft8020.philliesflowers.repositories.CustomerRepo;
+import java.util.List;
 
 @Controller
 public class Controllers {
-	
+
 	@Autowired
 	CustomerRepo customerRepo;
-	
+
 	@GetMapping("/")
 	public String doWelcomeWithParams(Model model) {
 		return "index";
 	}
-	
+
+
 	// An example of using a path variable.
 	// localhost:8081/usingParameter?name=Brian will add Brian to the welcome
 	// localhost:8081/usingParameter uses the default value of To You!
 	@GetMapping("/usingParameter")
-	public String doWelcomeWithParams(@RequestParam(value="name", defaultValue="To You!")String name, Model model)
-	{
+	public String doWelcomeWithParams(@RequestParam(value="name", defaultValue="To You!")String name, Model model) {
 		String sentence = "Welcome " + name;
 		model.addAttribute("message", sentence);
 		return "parameter";
 	}
-	
-	
+
+
 	// The repository uses the in-built findAll() method of MongoRepository This
 	// returns a list of People This list is added to the model The model is sent to
 	// the displayAll.html template.
@@ -44,8 +44,7 @@ public class Controllers {
 		return "displayAll";
 	}
 
-	
-	
+
 	// This uses a PathVariable to specify the id being searched for.
 	// findOne() is the default method to search for one record using MongoRepository.
 	// It returns one record.
@@ -57,8 +56,8 @@ public class Controllers {
 		model.addAttribute("customer", customer);
 		return "displayOne";
 	}
-	
-	
+
+
 }
 
 
@@ -78,4 +77,3 @@ public class Controllers {
 
 
 
- 
